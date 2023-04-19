891ece4a94cbad50d17c55fa4df025599fd82cb0
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-19 23:14:22 +0600

    firebase again auth initialization

---
+++ fire-auth-again/src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCwGaKuZlr1KA3qr-HEVhcCXyaOEkikLWw",
+  authDomain: "simple-firebase-again-ae22e.firebaseapp.com",
+  projectId: "simple-firebase-again-ae22e",
+  storageBucket: "simple-firebase-again-ae22e.appspot.com",
+  messagingSenderId: "927441554365",
+  appId: "1:927441554365:web:5fd246923cc956dcf9e0d4",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
