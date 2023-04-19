8d0417b4987f21a4d469f2304fce3ef4d3b3a152
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-19 23:28:04 +0600

    google authentication

--- fire-auth-again/src/App.jsx
+++ fire-auth-again/src/App.jsx
@@ -1,10 +1,36 @@
 import { useState } from "react";
 import "./App.css";
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import app from "./firebase/firebase.config";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [user, setUser] = useState(null);
+  const googleProvider = new GoogleAuthProvider();
+  const auth = getAuth(app);
 
-  return <div className="App"></div>;
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const loggedUser = result.user;
+        setUser(loggedUser);
+        console.log(result);
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
+  };
+
+  return (
+    <div className="App">
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+
+      {user && (
+        <div>
+          <h3>Name: {user.displayName}</h3>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
