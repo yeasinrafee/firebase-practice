f2d2d07682e6bb501b430a4a2f4fec323fc135c7
yeasinrafee <mdyeasinrafi7@gmail.com>
2023-04-20 09:05:28 +0600

    Register page

--- email-pass-auth/src/components/Register/Register.jsx
+++ email-pass-auth/src/components/Register/Register.jsx
@@ -3,7 +3,26 @@ import React from "react";
 const Register = () => {
   return (
     <div>
-      <h2>Register</h2>
+      <h2>Please Register</h2>
+      <form>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Enter your email"
+        />
+        <br />
+        <br />
+        <input
+          type="password"
+          name="password"
+          id="password"
+          placeholder="Enter your password"
+        />
+        <br />
+        <br />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
